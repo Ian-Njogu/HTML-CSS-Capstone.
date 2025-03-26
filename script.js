1988462a621fd3b7622024f6c6e89c452b1b40d3
Ian Njogu <robustian16@gmail.com>
2025-03-26 19:20:46 +0300

    Added responsiveness and the mobile menu

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuButton = document.getElementById("menu-button");
+const mobileMenu = document.getElementById("mobile-menu");
+const logo = document.getElementById("logo");
+
+
+menuButton.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+logo.classList.toggle("hidden");
+});
